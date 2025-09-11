2e183b61def95bd15110bafb5c1d81087d8bec3b
segevis <157372110+segevis@users.noreply.github.com>
2025-09-11 22:06:13 +0300

    small fix

--- src/pages/Sketches.jsx
+++ src/pages/Sketches.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Lightbox from '../components/Lightbox.jsx'
 import '../styles/Headings.css';
 
-const images = ['s0.jpg','s1.jpg','s2.jpg','s3.jpg','s4.jpg','s5.jpg','s6.jpg','s7.jpg','s8.jpg','s9.jpg','s10.jpg','s11.jpg','s13.jpg','s14.jpg','s15.jpg','s16.jpg','s17.jpg','s18.jpg',]
+const images = ['s0.jpg','s1.jpg','s2.jpg','s3.jpg','s4.jpg','s5.jpg','s6.jpg','s7.jpg','s8.jpg','s9.jpg','s10.jpg','s11.jpg','s13.jpg','s14.jpg','s15.jpg','s16.jpg','s17.jpg','s18.jpg']
 
 export default function Sketches() {
     const [preview, setPreview] = useState(null)
